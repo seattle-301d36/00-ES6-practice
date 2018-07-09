3718b041cd434925b1e15560b2660d84eb7d27cb
MAX MCFARLAND <mmcfarland522@gmail.com>
2018-07-09 13:07:41 -0700

    added comments to app.js

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -36,8 +36,8 @@ function displayPics(){
   }
   console.log(rando);
   // TODO: In a sentence or two, explain why the previous line of code threw an error when we changed the variable declaration from `var to `let`.
-  // PUT YOUR RESPONSE IN THIS COMMENT
-  console.log(viewed)
+  //Because the while() construction loops through for (viewed.length), the code is trying to reassign a value to rando, which is not allowed with the let() command. This is because the let() command will set the variable to a constant within the scope of the block, which inherintly cannot be reassigned a value.
+  console.log(viewed);
 
   // To the DOM and beyond!
   for (var i = 0; i < 3; i++){
@@ -125,7 +125,7 @@ document.getElementById('bus').addEventListener('click', function(){
 
 if(localStorage.busmall){
   console.log('Local storage data exists');
-  allProducts = JSON.parse(localStorage.busmall)
+  allProducts = JSON.parse(localStorage.busmall);
 } else {
   console.log('There is no local storage data; initialize app by creating instances');
   for(var i = 0; i < names.length; i++) {
